dcf6c19992be1259c5cf8cc4862aa41bc7e2509a
Jenny Citherlet <jennycith@gmail.com>
2020-08-11 22:50:15 +0000

    Remove extra space

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,12 @@
 $(document).ready(function() {
-  //console.log("DOM is ready");
   $('textarea').keyup(function() {
     let charRemaining = 140 - $(this).val().length;
     let counter = $(this).siblings().children('.counter');
     if (charRemaining < 0) {
-      counter.css("color", "red")
+      counter.css("color", "red");
     } else {
       counter.removeAttr("style");
     }
     counter.html(charRemaining);
   });
 });
-
-// $(document).ready(function() {
-//   //console.log("DOM is ready");
-//   $('textarea').keyup(function() {
-//     let charRemaining = 140 - $(this).val().length;
-//     let counter = $(this).siblings().children('.counter');
-//     charRemaining < 0 ? counter.css("color", "red") : counter.removeAttr("style");
-//     counter.html(charRemaining);
-//   });
-// });
